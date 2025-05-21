972a4a0f672b832683532c61eef19d65e89934cc
whyuan00 <whyuan00@gmail.com>
2025-05-21 10:37:46 +0900

    Jdbc 메서드 수정

--- src/AnimalManager.java
+++ src/AnimalManager.java
@@ -1,11 +1,9 @@
 import db.JDBCImpls;
-import db.JDBCUtils;
 import java.util.Map;
 import java.util.Scanner;
 
 public class AnimalManager {
     public static Scanner sc;
-    public static JDBCUtils oj;
     public static Map<Integer, String> map;
 
     // 생성자에 바로 run 코드 추가
@@ -20,7 +18,7 @@ public class AnimalManager {
         System.out.println(">> 0. 종료");
     }
 
-
+    // 1번 테이블 내 전체 동물 조회 
     private void findAllAnimals() {
         System.out.println("-----------------------------------------------");
         System.out.println();
@@ -30,8 +28,7 @@ public class AnimalManager {
         System.out.println(">> 2. 조류 ");
         System.out.println(">> 3. 파충류 ");
         System.out.println(">> 4. 양서류 ");
-        System.out.println();
-        System.out.println("0. 종료");
+        System.out.println(">> 0. 종료");
 
         boolean isRun = true;
         sc = new Scanner(System.in);
@@ -40,10 +37,10 @@ public class AnimalManager {
             int num = sc.nextInt();
             JDBCImpls oj = new JDBCImpls();
             switch (num) {
-                case 1 -> oj.readAllData(TableNames.MAMMALS);
-                case 2 -> oj.readAllData(TableNames.BIRDS);
-                case 3 -> oj.readAllData(TableNames.REPTILES);
-                case 4 -> oj.readAllData(TableNames.AMPHIBIANS);
+                case 1 -> oj.readAllData("Mammals");
+                case 2 -> oj.readAllData("Birds");
+                case 3 -> oj.readAllData("Reptiles");
+                case 4 -> oj.readAllData("Amphibians");
                 case 0 -> isRun = false;
                 default -> System.out.println("잘못된 입력입니다");
             }
@@ -52,25 +49,46 @@ public class AnimalManager {
 
     }
 
-    private void addAnimalInfo() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addAnimalInfo'");
-    }
-
+    // 2번 특정 동물 정보 조회 
     private void findAnimal() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAnimal'");
+        // TODO: 나중에 삭제
+        // JDBCImpls oj = new JDBCImpls();
+        // System.out.println("사자 비슷한거 찾기");
+        // oj.readSimilarData("Mammals", "사자");
     }
 
+    // 3번 새로운 동물 정보 추가
+    private void addAnimalInfo() {
+        JDBCImpls oj = new JDBCImpls();
+        // TODO: 나중에 삭제
+
+        // System.out.println("포유류에 추가");
+        // oj.createData("Mammals", "이름", "o", "b", "l");
+
+        // 특정 정보 조회
+        // System.out.println("조회");
+        // oj.readOneData("Mammals", "캘리포니아바다사자");
+
+        // 조회 후 수정 (id로 가져와서 나머지 정보 수정)
+        // System.out.println("수정");
+        // oj.updateData("Mammals",110, "이름1", "o1", "b1", "l1");
+
+        // 삭제
+        // System.out.println("삭제");
+        // oj.deleteData("Mammals", "이름");
+
+    }
+    
     public void run() {
         boolean isRun = true;
 
         while (isRun) {
+
             Scanner sc = new Scanner(System.in);
             int num = sc.nextInt();
             switch (num) {
                 case 1:
-                    System.out.println("전체 동물 목록 조회"); // findAllAnimals
+                    System.out.println("전체 동물 목록 조회");
                     System.out.println();
                     findAllAnimals();
                     break;
--- src/db/JDBCImpls.java
+++ src/db/JDBCImpls.java
@@ -39,29 +39,65 @@ public class JDBCImpls {
             PreparedStatement pstmt = conn.prepareStatement(sql);
             ResultSet rs = pstmt.executeQuery();
 
+            System.out.println("-----------------------------------------------");
+            System.out.println();
+            System.out.printf("[ %s 정보 전체 조회 ]", tableName);
+            System.out.println();
+            
             while (rs.next()) {
                 String name = rs.getString("NAME");
                 String ordinal = rs.getString("ORDINAL");
                 String branch = rs.getString("BRANCH");
                 String location = rs.getString("LOCATION");
 
-                System.out.println("-----------------------------------------------");
-                System.out.println();
-                System.out.printf("[ %s 정보 전체 조회 ]", tableName);
-
                 System.out.printf("%s  ||  %s  ||  %s  에 위치 \n",
                         name, ordinal, branch, location);
-                System.out.println();
             }
 
         } catch (Exception e) {
-            System.out.println("data 조회회 과정에서 에러 발생");
+            System.out.println("data 조회 과정에서 에러 발생");
             e.printStackTrace();
         }
 
     };
 
-    // 비슷한 값 조회
+    // 이름으로 동일 정보 조회
+    public void readOneData(String tableName, String name) {
+        try {
+            String sql = String.format(
+                    "SELECT * FROM %s WHERE NAME LIKE ?",
+                    tableName);
+            Connection conn = JDBCUtils.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (!rs.next()) {
+                System.out.println("해당 이름의 동물 정보가 존재하지 않습니다");
+            } else {
+                System.out.println("-----------------------------------------------");
+                System.out.println();
+                System.out.printf("[ %s 동물 정보 조회 ]\n", name);
+                System.out.println();
+                do {
+                    String id = rs.getString("ID");
+                    String dataName = rs.getString("NAME");
+                    String ordinal = rs.getString("ORDINAL");
+                    String branch = rs.getString("BRANCH");
+                    String location = rs.getString("LOCATION");
+
+                    System.out.printf("ID: %s || %s || %s || %s 에 위치 \n",
+                            id, dataName, ordinal, branch, location);
+                } while (rs.next()); // 다음 행이 있으면 반복
+            }
+
+        } catch (Exception e) {
+            System.out.printf("%s 조회 과정에서 에러가 발생했습니다", name);
+            e.printStackTrace();
+        }
+    }
+
+    // 비슷한 이름름 조회
     public void readSimilarData(String tableName, String name) {
         try {
             String sql = String.format(
@@ -69,29 +105,33 @@ public class JDBCImpls {
                     tableName);
             Connection conn = JDBCUtils.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, "%"+name+"%");
+            pstmt.setString(1, "%" + name + "%");
             ResultSet rs = pstmt.executeQuery();
 
-            while (rs.next()) {
-                String dataName = rs.getString("NAME");
-                String ordinal = rs.getString("ORDINAL");
-                String branch = rs.getString("BRANCH");
-                String location = rs.getString("LOCATION");
-
+            if (!rs.next()) {
+                System.out.println("유사한 동물 정보가 존재하지 않습니다");
+            } else {
                 System.out.println("-----------------------------------------------");
                 System.out.println();
-                System.out.printf("[ %s 과 유사한 값 조회 ]", dataName);
-
-                System.out.printf("%s  ||  %s  ||  %s  에 위치 \n",
-                        dataName, ordinal, branch, location);
+                System.out.printf("[ %s 과 유사한 동물 정보 조회 ]\n", name);
                 System.out.println();
+                do {
+                    String id = rs.getString("ID");
+                    String dataName = rs.getString("NAME");
+                    String ordinal = rs.getString("ORDINAL");
+                    String branch = rs.getString("BRANCH");
+                    String location = rs.getString("LOCATION");
+
+                    System.out.printf("ID: %s || %s || %s || %s 에 위치 \n",
+                            id, dataName, ordinal, branch, location);
+                } while (rs.next()); // 다음 행이 있으면 반복
             }
 
         } catch (Exception e) {
         }
     }
 
-    // 삭제
+    // 이름으로 삭제
     public void deleteData(String tableName, String name) {
         try {
             String sql = String.format("DELETE FROM %s WHERE NAME = ?", tableName);
@@ -109,7 +149,7 @@ public class JDBCImpls {
         }
     }
 
-    // 수정
+    // id 로 찾아서 수정
     public void updateData(String tableName, int id, String name, String ordinal, String branch, String location) {
         try {
             String sql = String.format(
