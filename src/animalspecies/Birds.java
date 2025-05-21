ba8f71cfbe1568cd94376670111a974e0c5c2699
https://github.com/usrnmjwn/animalPjt.git <ean40art275@gmail.com>
2025-05-21 16:23:44 +0900

    #조류 클래스 추가 조류 클래스를 추가해보았습니다.

---
+++ src/animalspecies/Birds.java
@@ -0,0 +1,17 @@
+package animalspecies;
+
+public class Birds {
+    public int id;
+    public String name;
+    public String ordinal;
+    public String branch;
+    public String location;
+
+    public Birds (int id, String name, String ordinal, String branch, String location) {
+        this.id = id;
+        this.name = name;
+        this.ordinal = ordinal;
+        this.branch = branch;
+        this.location = location;
+    }
+}
