5d8008fbbc8965ca1a1560678a4109014fb34b67
Seonjae <bbodaewall@gmail.com>
2025-05-21 16:26:40 +0900

    🐸 Add: 실행코드 파일 구현

--- src/App.java
+++ src/App.java
@@ -1,9 +1,17 @@
+import animalspecies.Amphibians;
 
 public class App {
     public static void main(String[] args) {
+        // Amphibians 객체 생성
+        Amphibians frog = new Amphibians("두꺼비", "양서류", "두꺼비과", "양서류관");
 
-        AnimalManager animalManager = new AnimalManager();
-        animalManager.run();
+        // 정보 출력
+        System.out.println("=== 동물 정보 ===");
+        frog.showInfo();
 
+        // 행동 실행
+        System.out.println("\n=== 행동 실행 ===");
+        frog.walk();
+        frog.swim();
     }
 }
