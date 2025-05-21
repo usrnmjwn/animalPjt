d103574459126a7ebfebda3570230f7b230ee91a
hazoon37 <hazoon37@gmail.com>
2025-05-21 16:28:15 +0900

    #포유류 추가 애니멀스피시즈에 포유류 추가요

---
+++ src/animalspecies/Mammals.java
@@ -0,0 +1,17 @@
+package animalspecies;
+
+public class Mammals {
+    public int id;
+    public String name;
+    public String ordinal;
+    public String branch;
+    public String location;
+
+    public Mammals(int id, String name, String ordinal, String branch, String location) {
+        this.id = id;
+        this.name = name;
+        this.ordinal = ordinal;
+        this.branch = branch;
+        this.location = location;
+    }
+}
